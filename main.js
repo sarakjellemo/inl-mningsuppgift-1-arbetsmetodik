f0bcb2a7ffcb6a77e2ea84e3dfe950943284207c
sarakjellemo <77279576+sarakjellemo@users.noreply.github.com>
2021-03-24 13:41:42 +0100

    final code

--- main.js
+++ main.js
@@ -1,24 +1,24 @@
 //Kod 1 
 let cars = ['volvo', 'saab', 'bmw', 'tesla'];
 
-// for (let i = 0; i < cars.length; i++){
-//     console.log(cars[i]);
-// }
+for (let i = 0; i < cars.length; i++){
+    console.log(cars[i]);
+}
 
-// //Kod 2
-// let cars = ['volvo', 'saab', 'bmw', 'tesla'];
-// let fruits = ['apple', 'banana', 'orange'];
+//Kod 2
+let cars = ['volvo', 'saab', 'bmw', 'tesla'];
+let fruits = ['apple', 'banana', 'orange'];
 
-// myFunction();
+myFunction();
 
-// function myFunction(){
-//     for (let i = 0; i <cars.length; i++){
-//         console.log(cars[i]);
-//     }
-//     for (let i = 0; i <fruits.length; i++){
-//         console.log(fruits[i]);
-//     }
-// }
+function myFunction(){
+    for (let i = 0; i <cars.length; i++){
+        console.log(cars[i]);
+    }
+    for (let i = 0; i <fruits.length; i++){
+        console.log(fruits[i]);
+    }
+}
 
 //Kod 3 
 let flowers = ['rose', 'tulip', 'lily', 'orchid'];  
