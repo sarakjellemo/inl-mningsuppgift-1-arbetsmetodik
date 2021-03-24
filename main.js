7a4bd8f66dd5696fd1771e42969de7aec729892c
sarakjellemo <77279576+sarakjellemo@users.noreply.github.com>
2021-03-24 13:40:51 +0100

    cleaned up the code

--- main.js
+++ main.js
@@ -1,42 +1,34 @@
 //Kod 1 
-// let cars = ['volvo', 'saab', 'bmw', 'tesla'];
+let cars = ['volvo', 'saab', 'bmw', 'tesla'];
 
 // for (let i = 0; i < cars.length; i++){
 //     console.log(cars[i]);
 // }
 
-//Kod 2
-let cars = ['volvo', 'saab', 'bmw', 'tesla'];
-let fruits = ['apple', 'banana', 'orange'];
+// //Kod 2
+// let cars = ['volvo', 'saab', 'bmw', 'tesla'];
+// let fruits = ['apple', 'banana', 'orange'];
 
-myFunction();
+// myFunction();
 
-function myFunction(){
-    for (let i = 0; i <cars.length; i++){
-        console.log(cars[i]);
-    }
-    for (let i = 0; i <fruits.length; i++){
-        console.log(fruits[i]);
-    }
-}
-
-// for (let i = 0; i <cars.length; i++){
-//     console.log(cars[i]);
-// }
-
-// for (let i = 0; i <fruits.length; i++){
-//     console.log(fruits[i]);
+// function myFunction(){
+//     for (let i = 0; i <cars.length; i++){
+//         console.log(cars[i]);
+//     }
+//     for (let i = 0; i <fruits.length; i++){
+//         console.log(fruits[i]);
+//     }
 // }
 
 //Kod 3 
-// let flowers = ['rose', 'tulip', 'lily', 'orchid'];  
-// printFlowers(flowers);
+let flowers = ['rose', 'tulip', 'lily', 'orchid'];  
+printFlowers(flowers);
 
-// function printFlowers () {  
-//     for (let i = 0; i < flowers.length; i++) {        
-//         console.log(flowers[i]);      
-//     }    
-// }    
+function printFlowers () {  
+    for (let i = 0; i < flowers.length; i++) {        
+        console.log(flowers[i]);      
+    }    
+}    
 
 
 
