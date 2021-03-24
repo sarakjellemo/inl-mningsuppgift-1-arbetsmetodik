2abe9a1d8c9b6ffb6bd27b9058707292492f3a0c
sarakjellemo <77279576+sarakjellemo@users.noreply.github.com>
2021-03-24 13:25:41 +0100

    added kod3

--- main.js
+++ main.js
@@ -6,14 +6,26 @@
 // }
 
 //Kod 2
-let cars = ['volvo', 'saab', 'bmw', 'tesla'];
-let fruits = ['apple', 'banana', 'orange'];
+// let cars = ['volvo', 'saab', 'bmw', 'tesla'];
+// let fruits = ['apple', 'banana', 'orange'];
 
-for (let i = 0; i <cars.length; i++){
-    console.log(cars[i]);
-}
+// for (let i = 0; i <cars.length; i++){
+//     console.log(cars[i]);
+// }
 
-for (let i = 0; i <fruits.length; i++){
-    console.log(fruits[i]);
-}
+// for (let i = 0; i <fruits.length; i++){
+//     console.log(fruits[i]);
+// }
+
+//Kod 3 
+let flowers = ['rose', 'tulip', 'lily', 'orchid'];  
+printFlowers(flowers);
+
+function printFlowers () {  
+    for (let i = 0; i < flowers.length; i++) {        
+        console.log(flowers[i]);      
+    }    
+}    
+
+
 
